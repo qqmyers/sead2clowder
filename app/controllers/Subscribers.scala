308c3d8afb2c65e4a9d0dc663fba2507bb1feffc
c.sophocleous <c.sophocleous@CY_T_user01.cyi.ac.cy>
2014-01-24 15:13:53 +0200

    Unsubscription capability.

--- app/controllers/Subscribers.scala
+++ app/controllers/Subscribers.scala
@@ -2,10 +2,12 @@ package controllers
 
 import play.api.data.Form
 import play.api.data.Forms._
+import play.api.data.validation
 import models.Subscriber
 import api.WithPermission
 import api.Permission
 import play.api.Logger
+import play.api.mvc.Flash
 
 object Subscribers extends SecuredController {
 
@@ -21,6 +23,14 @@ object Subscribers extends SecuredController {
     ((name, surname, email) => Subscriber(name = name, surname = surname, email = email))
     ((subscriber: Subscriber) => Some((subscriber.name, subscriber.surname, subscriber.email)))
    )
+   
+   val unsubscriptionForm = Form(
+     single(
+      "email" -> email
+     )verifying("Subscriber with input email not found.", fields => fields match {
+     		case inputEmail => Subscriber.findOneByEmail(inputEmail).isDefined
+     	})
+  )
   
    def subscribe()  = SecuredAction(authorization=WithPermission(Permission.Public)) { implicit request =>
     implicit val user = request.user
@@ -29,7 +39,7 @@ object Subscribers extends SecuredController {
   
   def unsubscribe()  = SecuredAction(authorization=WithPermission(Permission.Public)) { implicit request =>
     implicit val user = request.user
-  	Ok(views.html.newSubscriber(subscriptionForm)) 
+  	Ok(views.html.removeSubscriber(unsubscriptionForm))
   }
   
   /**
@@ -41,16 +51,41 @@ object Subscribers extends SecuredController {
         subscriptionForm.bindFromRequest.fold(
           errors => BadRequest(views.html.newSubscriber(errors)),
 	      subscriber => {
-		        Logger.debug("Saving subscription " + subscriber.email)
+		        Logger.debug("Saving subscription with email " + subscriber.email)
 		        		     
 			        // TODO create a service instead of calling salat directly
 		            Subscriber.save(subscriber)
-
 		            // redirect to main page
 		            Redirect(routes.Application.index)
 			      } 
 	)
   }
+  
+  def removeSubscription() = SecuredAction(authorization=WithPermission(Permission.Public)) { implicit request =>
+    implicit val user = request.user
+    
+        unsubscriptionForm.bindFromRequest.fold(
+          errors => BadRequest(views.html.removeSubscriber(errors)),
+	      email => {
+		        Logger.debug("Deleting subscription with email " + email)
+		        		
+		        Subscriber.findOneByEmail(email) match{
+		          case Some(subscriber) => {
+		            // TODO create a service instead of calling salat directly
+		            Subscriber.remove(subscriber)
+		            // redirect to main page
+		            Redirect(routes.Application.index)
+		          }
+		          case None => {
+		            Logger.error("Subscriber with email " + email + " not found.")
+		            Redirect(routes.Application.index)
+		          }		          
+		        }		        
+		} 
+	)
+  }
+  
+  
   
    
 }
