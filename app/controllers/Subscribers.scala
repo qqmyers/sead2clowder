17e240a5a28357028db4edc331d09099a8a0f996
c.sophocleous <c.sophocleous@CY_T_user01.cyi.ac.cy>
2014-01-24 12:37:02 +0200

    Addition of new subscribers.

--- app/controllers/Subscribers.scala
+++ app/controllers/Subscribers.scala
@@ -5,6 +5,7 @@ import play.api.data.Forms._
 import models.Subscriber
 import api.WithPermission
 import api.Permission
+import play.api.Logger
 
 object Subscribers extends SecuredController {
 
@@ -15,15 +16,41 @@ object Subscribers extends SecuredController {
     mapping(
       "name" -> nonEmptyText,
       "surname" -> nonEmptyText,
-      "email" -> nonEmptyText
+      "email" -> email
     )
     ((name, surname, email) => Subscriber(name = name, surname = surname, email = email))
     ((subscriber: Subscriber) => Some((subscriber.name, subscriber.surname, subscriber.email)))
    )
   
-   def newSubscriber()  = SecuredAction(authorization=WithPermission(Permission.Public)) { implicit request =>
+   def subscribe()  = SecuredAction(authorization=WithPermission(Permission.Public)) { implicit request =>
     implicit val user = request.user
   	Ok(views.html.newSubscriber(subscriptionForm)) 
   }
+  
+  def unsubscribe()  = SecuredAction(authorization=WithPermission(Permission.Public)) { implicit request =>
+    implicit val user = request.user
+  	Ok(views.html.newSubscriber(subscriptionForm)) 
+  }
+  
+  /**
+   * Create subscription.
+   */
+  def submit() = SecuredAction(authorization=WithPermission(Permission.Public)) { implicit request =>
+    implicit val user = request.user
+    
+        subscriptionForm.bindFromRequest.fold(
+          errors => BadRequest(views.html.newSubscriber(errors)),
+	      subscriber => {
+		        Logger.debug("Saving subscription " + subscriber.email)
+		        		     
+			        // TODO create a service instead of calling salat directly
+		            Subscriber.save(subscriber)
+
+		            // redirect to main page
+		            Redirect(routes.Application.index)
+			      } 
+	)
+  }
+  
    
 }
