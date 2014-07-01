2127052083c6d03afd2f26c2e0ebbc33f267a442
c.sophocleous <c.sophocleous@CY_T_user01.cyi.ac.cy>
2014-07-01 17:30:47 +0300

    Addition and removal if access rights for individual resources for a user.

--- app/services/UserAccessRightsService.scala
+++ app/services/UserAccessRightsService.scala
@@ -1,6 +1,7 @@
 package services
 
 import models.UserPermissions
+import securesocial.core.Identity
 
 trait UserAccessRightsService {
 
@@ -9,4 +10,9 @@ trait UserAccessRightsService {
    */
   def initRights(userPermissions: UserPermissions): Option[String]
   
+  def get(user: Identity): Option[UserPermissions]
+  
+  def addPermissionLevel(user: Identity, resourceId: String, resourceType: String, permissionType: String)
+  
+  def removePermissionLevel(user: Identity, resourceId: String, resourceType: String, permissionType: String)
 }
--- app/services/mongodb/MongoDBUserAccessRightsService.scala
+++ app/services/mongodb/MongoDBUserAccessRightsService.scala
@@ -8,6 +8,8 @@ import com.novus.salat.dao.{ModelCompanion, SalatDAO}
 import com.mongodb.casbah.Imports._
 import MongoContext.context
 import play.api.Play.current
+import play.api.Logger
+import securesocial.core.Identity
 
 
 
@@ -22,6 +24,112 @@ class MongoDBUserAccessRightsService extends UserAccessRightsService {
 
   def initRights(userPermissions: UserPermissions): Option[String] = {  
     UserPermissions.insert(userPermissions).map(_.toString) 
+  }
+  
+  def get(user: Identity): Option[UserPermissions] = {
+    UserPermissions.findOne($and("email" -> user.email.getOrElse(""), "name" -> user.fullName))
+  }
+  
+  def addPermissionLevel(user: Identity, resourceId: String, resourceType: String, permissionType: String) = {
+    if(permissionType.equals("administrate")){
+      addPermission(user, resourceId, resourceType, "view")
+      addPermission(user, resourceId, resourceType, "modify")
+      addPermission(user, resourceId, resourceType, "administrate")
+    }else if(permissionType.equals("modify")){
+      addPermission(user, resourceId, resourceType, "view")
+      addPermission(user, resourceId, resourceType, "modify")
+    }else if(permissionType.equals("view")){
+      addPermission(user, resourceId, resourceType, "view")
+    }
+    else
+    	  Logger.error("Unknown permission type")  
+  }
+  
+  def addPermission(user: Identity, resourceId: String, resourceType: String, permissionType: String)  {
+    var updateAction: DBObject = null
+    if(resourceType.equals("file")){
+      if(permissionType.equals("view")){
+        updateAction = $addToSet("filesViewOnly" -> resourceId)
+      }else if(permissionType.equals("modify")){
+        updateAction = $addToSet("filesViewModify" -> resourceId)
+      }else if(permissionType.equals("administrate")){
+        updateAction = $addToSet("filesAdministrate" -> resourceId)
+      }     
+    }else if(resourceType.equals("dataset")){
+      if(permissionType.equals("view")){
+        updateAction = $addToSet("datasetsViewOnly" -> resourceId)
+      }else if(permissionType.equals("modify")){
+        updateAction = $addToSet("datasetsViewModify" -> resourceId)
+      }else if(permissionType.equals("administrate")){
+        updateAction = $addToSet("datasetsAdministrate" -> resourceId)
+      }
+    }else if(resourceType.equals("collection")){
+      if(permissionType.equals("view")){
+        updateAction = $addToSet("collectionsViewOnly" -> resourceId)
+      }else if(permissionType.equals("modify")){
+        updateAction = $addToSet("collectionsViewModify" -> resourceId)
+      }else if(permissionType.equals("administrate")){
+        updateAction = $addToSet("collectionsAdministrate" -> resourceId)
+      }
+    }
+    else{
+      Logger.error("Unknown resource type")
+      return
+    }
+    
+    UserPermissions.update(MongoDBObject("email" -> user.email.getOrElse(""), "name" -> user.fullName), updateAction, false, false, WriteConcern.Safe)
+    
+  }
+  
+  def removePermissionLevel(user: Identity, resourceId: String, resourceType: String, permissionType: String) = {
+    if(permissionType.equals("view")){
+      removePermission(user, resourceId, resourceType, "view")
+      removePermission(user, resourceId, resourceType, "modify")
+      removePermission(user, resourceId, resourceType, "administrate")
+    }else if(permissionType.equals("modify")){
+      removePermission(user, resourceId, resourceType, "administrate")
+      removePermission(user, resourceId, resourceType, "modify")
+    }else if(permissionType.equals("administrate")){
+      removePermission(user, resourceId, resourceType, "administrate")
+    }
+    else
+    	  Logger.error("Unknown permission type")  
+  }
+  
+  def removePermission(user: Identity, resourceId: String, resourceType: String, permissionType: String)  {
+    var updateAction: DBObject = null
+    if(resourceType.equals("file")){
+      if(permissionType.equals("view")){
+        updateAction = $pull("filesViewOnly" -> resourceId)
+      }else if(permissionType.equals("modify")){
+        updateAction = $pull("filesViewModify" -> resourceId)
+      }else if(permissionType.equals("administrate")){
+        updateAction = $pull("filesAdministrate" -> resourceId)
+      }     
+    }else if(resourceType.equals("dataset")){
+      if(permissionType.equals("view")){
+        updateAction = $pull("datasetsViewOnly" -> resourceId)
+      }else if(permissionType.equals("modify")){
+        updateAction = $pull("datasetsViewModify" -> resourceId)
+      }else if(permissionType.equals("administrate")){
+        updateAction = $pull("datasetsAdministrate" -> resourceId)
+      }
+    }else if(resourceType.equals("collection")){
+      if(permissionType.equals("view")){
+        updateAction = $pull("collectionsViewOnly" -> resourceId)
+      }else if(permissionType.equals("modify")){
+        updateAction = $pull("collectionsViewModify" -> resourceId)
+      }else if(permissionType.equals("administrate")){
+        updateAction = $pull("collectionsAdministrate" -> resourceId)
+      }
+    }
+    else{
+      Logger.error("Unknown resource type")
+      return
+    }
+    
+    UserPermissions.update(MongoDBObject("email" -> user.email.getOrElse(""), "name" -> user.fullName), updateAction, false, false, WriteConcern.Safe)
+    
   }
   
 }
