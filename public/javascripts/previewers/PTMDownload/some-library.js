f634cfad944460240ff638ac0dd8cb2c8168a59c
c.sophocleous <c.sophocleous@CY_T_user01.cyi.ac.cy>
2014-07-23 17:55:58 +0300

    Can download base PTMs and 3D models generated by imageset-to-PTM and PTM-to-3D extractors respectively.

---
+++ public/javascripts/previewers/PTMDownload/some-library.js
@@ -0,0 +1,19 @@
+function NavigateToSite(prNum){
+    var ddl = document.getElementById("ddlMyList");
+    var selectedVal = ddl.options[ddl.selectedIndex].value;
+
+    window.open(selectedVal)
+}
+
+(function ($, Configuration) {
+  console.log("PTM download interface for " + Configuration.id);
+  
+  console.log("Updating tab " + Configuration.tab);
+  
+  var prNum = Configuration.tab.replace("#previewer","");
+  window["download"+prNum]
+  
+  var hostAddress = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '')
+  $(Configuration.tab).append("<a href='" + hostAddress+ Configuration.url + "'>Download PTM file generated from imageset</a>");
+
+}(jQuery, Configuration));
