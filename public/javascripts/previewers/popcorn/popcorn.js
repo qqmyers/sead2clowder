725099d9bcbfbdb47eb8762293c1e760fbb25d6e
c.sophocleous <c.sophocleous@CY_T_user01.cyi.ac.cy>
2014-08-29 14:37:30 +0300

    Video previewers cleanup and activation of the Popcorn previewer, together with warning for using it on machines with a second monitor.

--- public/javascripts/previewers/popcorn/popcorn.js
+++ public/javascripts/previewers/popcorn/popcorn.js
@@ -1,6 +1,8 @@
 (function ($, Configuration) {
   console.log("video previewer for " + Configuration.id);
   
+  $(Configuration.tab).append("<br/><p><b>Important: </b>Do not use this previewer on computers using a second screen, as there are some issues. Use the Quicktime-based one instead.</p>");
+  
   var s = document.createElement("script");
   s.type = "text/javascript";
   s.src = "http://popcornjs.org/code/dist/popcorn-complete.min.js";
@@ -9,19 +11,8 @@
   $(Configuration.tab).append(s);  
   $(Configuration.tab).append("<br/>");
   $(Configuration.tab).append(			  
-     "<video width='600px' id='ourvideo' controls><source src='" + Configuration.url + "'></source></video>"
+     "<video width='750px' id='ourvideo' controls><source src='" + Configuration.url + "'></source></video>"
   );
-  
-//  var height = 400;  
-//  var width = 750;
-//  
-//  $(Configuration.tab).append("<p>For panoramic images viewed through Quicktime, Shift can be used to zoom in and Ctrl to zoom out.</p>");
-//    
-//  console.log("Updating tab " + Configuration.tab);
-//  $(Configuration.tab).append(s);  
-//  $(Configuration.tab).append(			  
-//     "<video width='600px' id='ourvideo' controls><source src='" + Configuration.url + "'></source></video>"
-//  );
-  
+    
   
 }(jQuery, Configuration));
