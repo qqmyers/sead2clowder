143c6c7a9934509483f148c99429b3240b2ba665
c.sophocleous <c.sophocleous@CY_T_user01.cyi.ac.cy>
2014-11-07 11:26:11 +0200

    Selecting presentation version (slides only, medium-quality, high-quality) based on gauging client's connection speed.

--- public/javascripts/previewers/presentation/some-library.js
+++ public/javascripts/previewers/presentation/some-library.js
@@ -2,43 +2,58 @@
   console.log("video presentation previewer for " + Configuration.id);  
   console.log("Updating tab " + Configuration.tab);
   
-  var pathJs = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '') + Configuration.jsPath + "/";
+  var hostAddress = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
+  var pathJs = hostAddress + Configuration.jsPath + "/";
+  var pathImages = hostAddress + Configuration.imagesPath + "/";
   
-  var s = document.createElement("script");
-  s.type = "text/javascript";
-  s.src = pathJs + "popcorn-complete.js";
-  $(Configuration.tab).append(s);
+  var useTab = Configuration.tab;
+  var referenceUrl = Configuration.url;
   
-  $.ajax({
-	    url: Configuration.url,
-	    async:false,
-	    success: function (data) {
-	    	  var videosIds = data.split("\n");
-	    		
-	    	  //Slides only
-	    	  $(Configuration.tab).append("<br/>");
-	    	  $(Configuration.tab).append(			  
-	    	     "<video width='750px' id='vid_"+videosIds[0]+"' controls><source src='" + jsRoutes.api.Previews.download(videosIds[0]).url  + "'></source></video>"
-	    	  );
-	    	  
-	    	  //Medium-quality
-	    	  $(Configuration.tab).append("<br/>");
-	    	  $(Configuration.tab).append(			  
-	    	     "<video width='750px' id='vid_"+videosIds[1]+"' controls><source src='" + jsRoutes.api.Previews.download(videosIds[1]).url  + "'></source></video>"
-	    	  );
-	    	  
-	    	  //High-quality
-	    	  $(Configuration.tab).append("<br/>");
-	    	  $(Configuration.tab).append(			  
-	    	     "<video width='750px' id='vid_"+videosIds[2]+"' controls><source src='" + jsRoutes.api.Previews.download(videosIds[2]).url  + "'></source></video>"
-	    	  );
-	    		    		    			    	
-	    	 },
-	    	 error: function(jqXHR, textStatus, errorThrown) { 
-	    	        alert("Status: " + textStatus); alert("Error: " + errorThrown); 
-	    	    },
-	    dataType: 'text'
-	});
+  var mediumQualityLimit = (Configuration.presentationsMediumQualityLimit > 0 ? Configuration.presentationsMediumQualityLimit : 256) * 1000;
+  var highQualityLimit = (Configuration.presentationsHighQualityLimit > 0 ? Configuration.presentationsHighQualityLimit : 512) * 1000;
+  //Calculate user's connection speed
+  var imageAddr = pathImages +"connectionSpeedTester.jpg" + "?n=" + Math.random();
+  var startTime, endTime;
+  var downloadSize = 5616998;
+  var download = new Image();
+  download.onload = function () {
+      endTime = (new Date()).getTime();
+      var duration = (endTime - startTime) / 1000;
+      var bitsLoaded = downloadSize * 8;
+      var speedbps = (bitsLoaded / duration).toFixed(2);
+      var usedVersion = 0;
+      //0=slides only, 1=medium-quality, 2=high-quality
+      if(speedbps >= highQualityLimit)
+    	  usedVersion = 2;
+      else if(speedbps >= mediumQualityLimit)
+    	  usedVersion = 1;
+      
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.src = pathJs + "popcorn-complete.js";
+      $(useTab).append(s);
+      
+      $.ajax({
+  	    url: referenceUrl,
+  	    async:true,
+  	    success: function (data) {
+  	    	  var videosIds = data.split("\n");
+  	    		
+  	    	  $(useTab).append("<br/>");
+  	    	  $(useTab).append(			  
+  	    	     "<video width='750px' id='vid_"+videosIds[usedVersion]+"' controls><source src='" + jsRoutes.api.Previews.download(videosIds[usedVersion]).url  + "'></source></video>"
+  	    	  );    		    			    	
+  	    	 },
+  	    	 error: function(jqXHR, textStatus, errorThrown) { 
+  	    	        alert("Status: " + textStatus); alert("Error: " + errorThrown); 
+  	    	    },
+  	    dataType: 'text'
+  	});
+      
+  }
+  startTime = (new Date()).getTime();
+  download.src = imageAddr;
   
 
 }(jQuery, Configuration));
+
