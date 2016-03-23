2526a8c02c814876bf1090094705c756fbac26b5
Todd Nicholson <tcnichol@illinois.edu>
2016-03-23 17:40:55 -0500

    removed some toString method

--- app/api/Vocabularies.scala
+++ app/api/Vocabularies.scala
@@ -260,7 +260,7 @@ class Vocabularies @Inject() (vocabularyService: VocabularyService, userService 
 
 
   def jsonVocabulary(vocabulary : Vocabulary): JsValue = {
-    toJson(Map("id" -> vocabulary.id.toString, "name" -> vocabulary.name.toString, "keys" -> vocabulary.keys.toString, "description" -> vocabulary.description.toString))
+    toJson(Map("id" -> vocabulary.id.toString, "name" -> vocabulary.name, "keys" -> vocabulary.keys.toString, "description" -> vocabulary.description))
   }
 
 }
