701325a5b445eaf2352d3fa26c423eb863fbce0d
Todd Nicholson <tcnichol@illinois.edu>
2016-06-12 16:23:23 -0500

    added class

--- app/api/T2C2.scala
+++ app/api/T2C2.scala
@@ -1,8 +1,49 @@
 package api
 
+import javax.inject.Inject
+import api.Permission._
+import com.wordnik.swagger.annotations.{ApiOperation, Api}
+import models.{Collection, ResourceRef, UUID}
+import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.Json.toJson
+import services.{CollectionService, DatasetService}
+
 /**
-  * Created by todd_n on 6/12/16.
+  * Created by todd_n on 4/21/16.
   */
-class T2C2 {
+@Api(value = "/api/t2c2", description = "Controller for t2c2 routes.")
+class T2C2 @Inject() (datasets : DatasetService, collections: CollectionService)  extends ApiController{
+
+  @ApiOperation(value = "List all datasets in a collection", notes = "Returns list of datasets and descriptions.", responseClass = "None", httpMethod = "GET")
+  def getDatasetsInCollectionWithColId(collectionId : UUID) = PermissionAction(Permission.ViewCollection, Some(ResourceRef(ResourceRef.collection, collectionId))){implicit request=>
+    var datasets_incollection = datasets.listCollection(collectionId.stringify)
+
+    var dataset_name_collectionid = for (dataset <- datasets_incollection)
+      yield(Json.obj("id"->dataset.id,"name"->dataset.name,"collection"->collectionId))
+    Ok(toJson(dataset_name_collectionid))
+  }
+
+  @ApiOperation(value = "Get all collections with dataset ids",
+    notes = "",
+    responseClass = "None", httpMethod = "GET")
+  def getAllCollectionsWithDatasetIds() = PermissionAction(Permission.ViewCollection) { implicit request =>
+    implicit val user = request.user
+    var count : Long  = collections.countAccess(Set[Permission](Permission.ViewCollection),user,true);
+    var limit = count.toInt
+    val all_collections_list = for (collection <- collections.listAccess(limit,Set[Permission](Permission.ViewCollection),request.user,false))
+      yield jsonCollection(collection)
+    Ok(toJson(all_collections_list))
+  }
+
+  def jsonCollection(collection: Collection): JsValue = {
+    var datasetsInCollection = datasets.listCollection(collection.id.stringify)
+    var datasetIds = for (dataset<-datasetsInCollection)
+      yield (dataset.name +":"+ dataset.id)
+    toJson(Map("id" -> collection.id.toString, "name" -> collection.name, "description" -> collection.description,
+      "created" -> collection.created.toString,"author"-> collection.author.toString, "root_flag" -> collection.root_flag.toString,
+      "child_collection_ids"-> collection.child_collection_ids.toString, "parent_collection_ids" -> collection.parent_collection_ids.toString,
+      "dataset_ids"->datasetIds.mkString(","),
+      "childCollectionsCount" -> collection.childCollectionsCount.toString, "datasetCount"-> collection.datasetCount.toString, "spaces" -> collection.spaces.toString))
+  }
 
 }
--- app/controllers/T2C2.scala
+++ app/controllers/T2C2.scala
@@ -1,8 +1,112 @@
 package controllers
 
-/**
-  * Created by todd_n on 6/12/16.
-  */
-class T2C2 {
+import java.text.SimpleDateFormat
+import java.util.Date
+import javax.inject.{Inject, Singleton}
+
+import api.Permission
+import models._
+import play.api.Logger
+import play.api.Play.current
+import services.{CollectionService, DatasetService, _}
+import util.RequiredFieldsConfig
+
+import scala.collection.immutable.List
+import scala.collection.mutable.ListBuffer
+
+@Singleton
+class T2C2 @Inject()(vocabularies : VocabularyService, datasets: DatasetService, collections: CollectionService, previewsService: PreviewService,
+                     spaceService: SpaceService, users: UserService, events: EventService) extends SecuredController {
+
+  def newTemplate(space: Option[String]) = PermissionAction(Permission.CreateCollection) { implicit request =>
+    implicit val user = request.user
+    val spacesList = user.get.spaceandrole.map(_.spaceId).flatMap(spaceService.get(_))
+    var decodedSpaceList = new ListBuffer[models.ProjectSpace]()
+    for (aSpace <- spacesList) {
+      //For each space in the list, check if the user has permission to add something to it, if so
+      //decode it and add it to the list to pass back to the view.
+      if (Permission.checkPermission(Permission.AddResourceToSpace, ResourceRef(ResourceRef.space, aSpace.id))) {
+        decodedSpaceList += Utils.decodeSpaceElements(aSpace)
+      }
+    }
+    space match {
+      case Some(spaceId) => {
+        spaceService.get(UUID(spaceId)) match {
+          case Some(s) => Ok(views.html.newTemplate(null, decodedSpaceList.toList, RequiredFieldsConfig.isNameRequired, RequiredFieldsConfig.isDescriptionRequired, Some(spaceId)))
+          case None => Ok(views.html.newTemplate(null, decodedSpaceList.toList, RequiredFieldsConfig.isNameRequired, RequiredFieldsConfig.isDescriptionRequired, None))
+        }
+      }
+      case None =>  Ok(views.html.newTemplate(null, decodedSpaceList.toList, RequiredFieldsConfig.isNameRequired, RequiredFieldsConfig.isDescriptionRequired, None))
+    }
+
+  }
+
+  def submit() = PermissionAction(Permission.CreateVocabulary)(parse.multipartFormData) { implicit request =>
+    Logger.debug("------- in Collections.submit ---------")
+    val colName = request.body.asFormUrlEncoded.getOrElse("name", null)
+    val colKeys = request.body.asFormUrlEncoded.getOrElse("keys", null)
+    val colDesc = request.body.asFormUrlEncoded.getOrElse("description", null)
+    val colSpace = request.body.asFormUrlEncoded.getOrElse("space", List.empty)
+
+    implicit val user = request.user
+    user match {
+      case Some(identity) => {
+        if (colName == null || colKeys == null || colDesc == null || colSpace == null) {
+          val spacesList = spaceService.list()
+          var decodedSpaceList = new ListBuffer[models.ProjectSpace]()
+          for (aSpace <- spacesList) {
+            decodedSpaceList += Utils.decodeSpaceElements(aSpace)
+          }
+          //This case shouldn't happen as it is validated on the client.
+          BadRequest(views.html.newVocabulary("Name, Description, or Space was missing during vocabulary creation.", decodedSpaceList.toList, RequiredFieldsConfig.isNameRequired, RequiredFieldsConfig.isDescriptionRequired, None))
+        }
+
+        var vocabulary: Vocabulary = null
+        if (colSpace.isEmpty || colSpace(0) == "default" || colSpace(0) == "") {
+          vocabulary = Vocabulary(name = colName(0).toString, keys = colKeys(0).split(',').toList, description = colDesc(0).split(',').toList, created = new Date, author = Some(identity))
+        }
+        else {
+          val stringSpaces = colSpace(0).split(",").toList
+          val colSpaces: List[UUID] = stringSpaces.map(aSpace => if (aSpace != "") UUID(aSpace) else None).filter(_ != None).asInstanceOf[List[UUID]]
+          vocabulary = Vocabulary(name = colName(0).toString, keys = colKeys(0).split(',').toList, description = colDesc(0).split(',').toList, created = new Date, author = Some(identity),spaces = colSpaces)
+        }
+
+        Logger.debug("Saving vocabulary " + vocabulary.name)
+        vocabularies.insert(vocabulary)
+        vocabulary.spaces.map {
+          sp => spaceService.get(sp) match {
+            case Some(s) => {
+              vocabularies.addToSpace(vocabulary.id, s.id)
+              //events.addSourceEvent(request.user, collection.id, collection.name, s.id, s.name, "add_collection_space")
+            }
+            case None => Logger.error(s"space with id $sp on collection $vocabulary.id doesn't exist.")
+          }
+        }
+
+        //index collection
+        val dateFormat = new SimpleDateFormat("dd/MM/yyyy")
+        //current.plugin[ElasticsearchPlugin].foreach{_.index("data", "vocabulary", vocabulary.id,
+        //List(("name",vocabulary.name), ("description", vocabulary.description), ("created",dateFormat.format(new Date())))}
+
+        //Add to Events Table
+        val option_user = users.findByIdentity(identity)
+        events.addObjectEvent(option_user, vocabulary.id, vocabulary.name, "create_vocabulary")
+
+        // redirect to collection page
+        current.plugin[AdminsNotifierPlugin].foreach {
+          _.sendAdminsNotification(Utils.baseUrl(request), "Collection", "added", vocabulary.id.toString, vocabulary.name)
+        }
+        Ok("not finished yet")
+      }
+      case None => Redirect(routes.Collections.list()).flashing("error" -> "You are not authorized to create new collections.")
+    }
+  }
+
+
+  /**
+   * Vocabulary.
+   */
+  def vocabulary(id: UUID) = PermissionAction(Permission.ViewVocabulary, Some(ResourceRef(ResourceRef.vocabulary, id))){implicit request =>
+    implicit val user = request.user
 
 }
