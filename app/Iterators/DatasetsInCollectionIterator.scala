02ddc173577f7af4932a49974ed52bb5f0783203
Todd Nicholson <tcnichol@illinois.edu>
2016-11-11 22:29:20 -0600

    using Option[Dataset] for currentDataset works now for empty colllection

--- app/Iterators/DatasetsInCollectionIterator.scala
+++ app/Iterators/DatasetsInCollectionIterator.scala
@@ -15,20 +15,29 @@ class DatasetsInCollectionIterator(pathToFolder : String, collection : models.Co
                                   datasets : DatasetService, files : FileService, folders : FolderService, metadataService : MetadataService,
                                    spaces : SpaceService) extends Iterator[Option[InputStream]] {
 
+
   def getDatasetsInCollection(collection : models.Collection,user : User) : List[Dataset] = {
     var datasetsInCollection : ListBuffer[Dataset] = ListBuffer.empty[Dataset]
     var datasetsInCollectionList = datasets.listCollection(collection.id.stringify,Some(user))
     datasetsInCollectionList
   }
 
+
+
   val datasetsInCollection = getDatasetsInCollection(collection, user.get)
 
   var datasetCount = 0
   val numDatasets = datasetsInCollection.size
 
-  var currentDataset = datasetsInCollection(datasetCount)
+  var currentDataset : Option[Dataset] = if (numDatasets > 0){
+    Some(datasetsInCollection(datasetCount))
+  } else {
+    None
+  }
+
   var currentDatasetIterator : Option[DatasetIterator]  = if (numDatasets > 0){
-    Some(new DatasetIterator(pathToFolder+"/"+currentDataset.name,currentDataset, zip, md5Files,
+
+    Some(new DatasetIterator(pathToFolder+"/"+currentDataset.get.name,currentDataset.get, zip, md5Files,
     folders, files,metadataService,datasets,spaces))
   } else {
     None
@@ -44,8 +53,8 @@ class DatasetsInCollectionIterator(pathToFolder : String, collection : models.Co
         } else {
           if (datasetCount < numDatasets -1){
             datasetCount +=1
-            currentDataset = datasetsInCollection(datasetCount)
-            currentDatasetIterator = Some(new DatasetIterator(pathToFolder+"/"+currentDataset.name,currentDataset, zip, md5Files,
+            currentDataset = Some(datasetsInCollection(datasetCount))
+            currentDatasetIterator = Some(new DatasetIterator(pathToFolder+"/"+currentDataset.get.name,currentDataset.get, zip, md5Files,
               folders, files,metadataService,datasets,spaces))
             true
           } else
