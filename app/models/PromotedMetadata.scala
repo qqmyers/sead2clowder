cbfe8e098b48acf117f8c284ae009673a03831e6
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2017-09-15 12:03:10 -0500

    Added reads and writes to promoted metadata model

--- app/models/PromotedMetadata.scala
+++ app/models/PromotedMetadata.scala
@@ -1,6 +1,7 @@
 package models
 
-import play.api.libs.json.JsValue
+import play.api.libs.json.{JsPath, Reads, Json, JsValue}
+import play.api.libs.functional.syntax._
 
 /**
   * Promoted extractor-generated metadata fields
@@ -10,8 +11,21 @@ case class PromotedMetadata (
   spaceId: Option[UUID] = None,
   json: JsValue
     /*
-      uri: String, // metadata field URL
+      uri: String, // metadata field URI (Also, it's unique name that includes hostname, exractor name )
       label: String, // metadata field name
       type: String // metadata field type
     */
 )
+
+object PromotedMetadata {
+
+  implicit val promotedMetadataFormat = Json.format[PromotedMetadata]
+
+  implicit val promotedMetadataReads: Reads[PromotedMetadata] = (
+    (JsPath \ "id").read[UUID] and
+      (JsPath \ "space_id").read[Option[UUID]] and
+      (JsPath \ "json").read[JsValue]
+    )(PromotedMetadata.apply _)
+
+  implicit val promotedMetadataWrites = Json.writes[PromotedMetadata]
+}
