f7badb49556c588059a1818a777d840d38ff3898
indiragp <indiragp@illinois.edu>
2017-11-03 09:46:26 -0500

    Using smaller logo

--- doc/src/sphinx/conf.py
+++ doc/src/sphinx/conf.py
@@ -154,7 +154,7 @@ html_theme = 'agogo'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "../../../public/images/logo_sm.png"
+html_logo = "../../../public/images/logo_60.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
